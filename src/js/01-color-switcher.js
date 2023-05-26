df83955c410cb966eb6445c37355f7e91b8b32ea
N0T4 <118122479+N0T4@users.noreply.github.com>
2023-05-26 12:47:17 +0100

    1
    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+const body = document.querySelector('body')
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+
+let TimerId = null;
+
+btnStart.addEventListener('click', () => {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+
+    TimerId = setInterval(event => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+})
+
+btnStop.addEventListener('click', () => {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+
+    clearInterval(TimerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
